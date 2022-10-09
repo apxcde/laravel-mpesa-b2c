fa4fabd5e1248f70796324323349f421ea7139fd
Mwamodo Erick <erick@mwamodo.com>
2022-10-09 20:59:23 +0300

    WIP

--- config/mpesa-b2c.php
+++ config/mpesa-b2c.php
@@ -1,6 +1,23 @@
 <?php
 
-// config for Apxcde/LaravelMpesaB2c
 return [
+    'env' => env('MPESA_ENV', 'sandbox'),
 
+    "shortcode" => env('MPESA_SHORTCODE', '600980'),
+
+    "key" => env('MPESA_KEY', 'IWBJnpHUSMqGLVU21qhxFOdfTOzGjH5a'),
+
+    "secret" => env('MPESA_SECRET', 'uqUYObhDprZoWFnG'),
+
+    "username" => env('MPESA_USERNAME', 'testapi'),
+
+    "password" => env('MPESA_PASSWORD', 'Safaricom980!'),
+
+    'results_url' => env('MPESA_RESULTS_URL', ''),
+
+    'timeout_url' => env('MPESA_TIMEOUT_URL', ''),
+
+    'generated_password' => env('MPESA_GENERATED_PASSWORD', 'UNPMpfrhSfSeqN566HAlAQYaIQMeLvpEPZ5SiUR5pJn4faGYBnye251wCLGR56B3uOtT39UmoSeHtFhIa3torjhkXsfESm5NvKhIIOnHKa5Ry3rzeVxL+ruZE2st80HCLsbsJUQmvJ8vbE+h+NamH4DJi7JFHrHAPJ06BPjZuQEYbd/Lei1q4sdmQg6c38ZAnPIrvvWWidqxWc+uspbjqC+Dcyy6o9uwkfCCYGkvLtA8n2FM8MZazh/wgVjBOSV/RMmnt/cZjqoAiUVTkW6FMac77w1ejhweN4khV9mhmZvjmfaFmYi54nXbLSOC8FvkyiJf8uecNSAyWb5G/IhpaQ=='),
+
+    'party_public_name' => env('MPESA_PARTY_PUBLIC_NAME', '4'),
 ];
--- src/LaravelMpesaB2cServiceProvider.php
+++ src/LaravelMpesaB2cServiceProvider.php
@@ -10,11 +10,6 @@ class LaravelMpesaB2cServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-mpesa-b2c')
             ->hasConfigFile()
