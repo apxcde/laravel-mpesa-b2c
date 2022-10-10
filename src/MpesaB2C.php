7983bd898cac457f8fc88b1b5409c4ef11667561
mwamodo <mwamodo@users.noreply.github.com>
2022-10-10 11:50:20 +0000

    Fix styling

--- src/MpesaB2C.php
+++ src/MpesaB2C.php
@@ -4,32 +4,32 @@ namespace Apxcde\LaravelMpesaB2c;
 
 class MpesaB2C extends Service
 {
-    public static function send($phone_number, int $amount, string $command, string $remarks, $occasion = "", $callback = null)
+    public static function send($phone_number, int $amount, string $command, string $remarks, $occasion = '', $callback = null)
     {
         $env = parent::$config->env;
-        $phone_number = ($env == "live") ? parent::formatPhoneNumber($phone_number) : "254708374149";
+        $phone_number = ($env == 'live') ? parent::formatPhoneNumber($phone_number) : '254708374149';
 
-        $endpoint = ($env == "live")
-            ? "https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest"
-            : "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest";
+        $endpoint = ($env == 'live')
+            ? 'https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest'
+            : 'https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest';
 
         $plaintext = parent::$config->password;
-        $publicKey = file_get_contents(__DIR__ . "/certs/{$env}/cert.cer");
+        $publicKey = file_get_contents(__DIR__."/certs/{$env}/cert.cer");
 
         openssl_public_encrypt($plaintext, $encrypted, $publicKey, OPENSSL_PKCS1_PADDING);
-        $password = ($env == "live") ? base64_encode($encrypted) : config('mpesa.generated_password');
+        $password = ($env == 'live') ? base64_encode($encrypted) : config('mpesa.generated_password');
 
         $curl_post_data = [
-            "InitiatorName" => parent::$config->username,
-            "SecurityCredential" => $password,
-            "CommandID" => $command,
-            "Amount" => round($amount),
-            "PartyA" => parent::$config->shortcode,
-            "PartyB" => $phone_number,
-            "Remarks" => $remarks,
-            "QueueTimeOutURL" => parent::$config->timeout_url,
-            "ResultURL" => parent::$config->results_url,
-            "Occasion" => $occasion,
+            'InitiatorName' => parent::$config->username,
+            'SecurityCredential' => $password,
+            'CommandID' => $command,
+            'Amount' => round($amount),
+            'PartyA' => parent::$config->shortcode,
+            'PartyB' => $phone_number,
+            'Remarks' => $remarks,
+            'QueueTimeOutURL' => parent::$config->timeout_url,
+            'ResultURL' => parent::$config->results_url,
+            'Occasion' => $occasion,
         ];
 
         $response = parent::post($endpoint, $curl_post_data);
--- src/Service.php
+++ src/Service.php
@@ -8,18 +8,18 @@ class Service
 
     public static function init($configs): void
     {
-        $base = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . ($_SERVER["SERVER_NAME"] ?? '');
+        $base = (isset($_SERVER['HTTPS']) ? 'https' : 'http').'://'.($_SERVER['SERVER_NAME'] ?? '');
 
         $defaults = [
-            "env" => config('mpesa.env'),
-            "type" => 4,
-            "shortcode" => config('mpesa.shortcode'),
-            "key" => config('mpesa.key'),
-            "secret" => config('mpesa.secret'),
-            "username" => config('mpesa.username'),
-            "password" => config('mpesa.password'),
-            "results_url" => config('mpesa.results_url'),
-            "timeout_url" => config('mpesa.timeout_url'),
+            'env' => config('mpesa.env'),
+            'type' => 4,
+            'shortcode' => config('mpesa.shortcode'),
+            'key' => config('mpesa.key'),
+            'secret' => config('mpesa.secret'),
+            'username' => config('mpesa.username'),
+            'password' => config('mpesa.password'),
+            'results_url' => config('mpesa.results_url'),
+            'timeout_url' => config('mpesa.timeout_url'),
         ];
 
         foreach ($defaults as $key => $value) {
@@ -33,22 +33,22 @@ class Service
 
     public static function formatPhoneNumber($phone_number)
     {
-        $phone_number = (str_starts_with($phone_number, "+")) ?
-            str_replace("+", "", $phone_number) :
+        $phone_number = (str_starts_with($phone_number, '+')) ?
+            str_replace('+', '', $phone_number) :
             $phone_number;
 
-        $phone_number = (str_starts_with($phone_number, "0")) ?
-            preg_replace("/^0/", "254", $phone_number) :
+        $phone_number = (str_starts_with($phone_number, '0')) ?
+            preg_replace('/^0/', '254', $phone_number) :
             $phone_number;
 
-        return (str_starts_with($phone_number, "7")) ? "254{$phone_number}" : $phone_number;
+        return (str_starts_with($phone_number, '7')) ? "254{$phone_number}" : $phone_number;
     }
 
     public static function get($endpoint, $credentials = null): bool|string
     {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $endpoint);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, ["Authorization: Basic " . $credentials]);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Authorization: Basic '.$credentials]);
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -58,15 +58,15 @@ class Service
 
     public static function token(): string
     {
-        $endpoint = (self::$config->env == "live")
-            ? "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
-            : "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+        $endpoint = (self::$config->env == 'live')
+            ? 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+            : 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 
-        $credentials = base64_encode(self::$config->key . ":" . self::$config->secret);
+        $credentials = base64_encode(self::$config->key.':'.self::$config->secret);
         $response = self::get($endpoint, $credentials);
         $result = json_decode($response);
 
-        return $result->access_token ?? "";
+        return $result->access_token ?? '';
     }
 
     public static function post($endpoint, $data = []): bool|string
@@ -83,8 +83,8 @@ class Service
             $curl,
             CURLOPT_HTTPHEADER,
             [
-                "Content-Type:application/json",
-                "Authorization:Bearer " . $token,
+                'Content-Type:application/json',
+                'Authorization:Bearer '.$token,
             ]
         );
 
@@ -93,47 +93,47 @@ class Service
 
     public static function reconcile(callable $callback = null): array
     {
-        $response = json_decode(file_get_contents("php://input"), true);
+        $response = json_decode(file_get_contents('php://input'), true);
 
         if (is_null($callback)) {
             return [
-                "ResultCode" => 0,
-                "ResultDesc" => "Service request successful",
+                'ResultCode' => 0,
+                'ResultDesc' => 'Service request successful',
             ];
         } else {
             return call_user_func_array($callback, [$response])
                 ? [
-                    "ResultCode" => 0,
-                    "ResultDesc" => "Service request successful",
+                    'ResultCode' => 0,
+                    'ResultDesc' => 'Service request successful',
                 ]
                 : [
-                    "ResultCode" => 1,
-                    "ResultDesc" => "Service request failed",
+                    'ResultCode' => 1,
+                    'ResultDesc' => 'Service request failed',
                 ];
         }
     }
 
-    public static function balance($command = "AccountBalance", $remarks = "Balance Query", $callback = null)
+    public static function balance($command = 'AccountBalance', $remarks = 'Balance Query', $callback = null)
     {
         $env = self::$config->env;
         $plaintext = self::$config->password;
-        $publicKey = file_get_contents(__DIR__ . "/certs/{$env}/cert.cer");
+        $publicKey = file_get_contents(__DIR__."/certs/{$env}/cert.cer");
 
         openssl_public_encrypt($plaintext, $encrypted, $publicKey, OPENSSL_PKCS1_PADDING);
-        $password = ($env == "live") ? base64_encode($encrypted) : config('mpesa.generated_password');
+        $password = ($env == 'live') ? base64_encode($encrypted) : config('mpesa.generated_password');
 
         $payload = [
-            "CommandID" => $command,
-            "PartyA" => self::$config->shortcode,
-            "IdentifierType" => self::$config->type,
-            "Initiator" => self::$config->username,
-            "SecurityCredential" => $password,
-            "Remarks" => $remarks,
-            "QueueTimeOutURL" => self::$config->timeout_url,
-            "ResultURL" => self::$config->results_url,
+            'CommandID' => $command,
+            'PartyA' => self::$config->shortcode,
+            'IdentifierType' => self::$config->type,
+            'Initiator' => self::$config->username,
+            'SecurityCredential' => $password,
+            'Remarks' => $remarks,
+            'QueueTimeOutURL' => self::$config->timeout_url,
+            'ResultURL' => self::$config->results_url,
         ];
 
-        $response = self::post("/accountbalance/v1/query", $payload);
+        $response = self::post('/accountbalance/v1/query', $payload);
         $result = json_decode($response, true);
 
         return is_null($callback)
